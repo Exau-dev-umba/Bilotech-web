28905ddc647ae5203befd4c72194f119072b040b
Nicolas.adeito <nicolasadeito1@gmail.com>
2023-05-12 17:31:14 +0100

    Api pr voir,creer,envoyer,recevoir,supprimer message

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Message;
+use App\Models\MessageModel;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthManager;
+use Illuminate\Support\Facades\Auth;
+use App\Repository\MessageRepository;
+use App\Http\Resources\MessageResource;
+use App\Http\Resources\ConversationResource;
+
+class 
+MessageController extends Controller
+{
+    private $r;
+    private $auth;
+
+    public function __construct(MessageRepository $conversationRepository, AuthManager $auth)
+    {
+        $this->r = $conversationRepository;
+        $this->auth = $auth;
+        
+    }
+    public function index (){
+
+
+       /*return view('conversations/index',[
+            'users'=> $this->r->getConversations($this->auth->user()->id)
+        ]);*/
+        $Messages= MessageResource::collection(MessageModel::paginate(4));
+    return response()->json($Messages);
+
+    }
+
+    public function show (Request $request, $id){
+
+        $message= new MessageResource(MessageModel::find($id));
+        return response()->json($message);
+
+
+    }
+    public function store( Request $request){
+        $message= MessageModel::create([
+            'content'=>$request->content,
+            'article_id'=>$request->article_id,
+            "from_id" => 1 // TODO: remplacer par le user connecté
+        ]);
+        $Message = new  MessageResource($message);
+        return response()->json($Message);
+    }
+
+    public function update(Request $request, $id){
+        $Message= MessageModel::find($id)->update([
+            'content'=>$request->name,
+            'article_id'=>$request->article_id,
+            "from_id"=>4
+        ]);
+        if($Message):
+            $result = MessageModel::find($id);
+            $Message = new MessageResource($result);
+            return response()->json($Message);
+        else:
+            return response()->json(["message"=>"Echec de modification"]);
+        endif;
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
