4157b21f9500c3dfbd3555e208bd17b7fb847648
espoir kakesa <espoirkakesa2@gmail.com>
2023-05-16 15:03:28 +0100

    modification fichier web

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::get('/', function () {
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
 Auth::routes();
 
+Route::resource('roles', App\Http\Controllers\RoleController::class);
