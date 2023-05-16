6fd8649eeb4eaba4205dbe8f764bd4880e9087b7
espoir kakesa <espoirkakesa2@gmail.com>
2023-05-16 15:05:11 +0100

    ajout fichier RolrCtrl

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::latest()->paginate(4);
+    
+        return view('roles.index',compact('roles'))
+            ->with('i', (request()->input('page', 1) - 1) * 4);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('roles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+
+        ]);
+    
+        Role::create($request->all());
+     
+        return redirect()->route('roles.index')
+                        ->with('success','Role créé avec succès.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Role $role)
+    {
+        return view('roles.show',compact('role'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Role $role)
+    {
+        $modelFiles = File::files(app_path('Models'));
+        $models = [];
+        foreach ($modelFiles as $file) {
+            $models[] = pathinfo($file)['filename'];
+        }
+        return view('roles.edit', ['models' => $models], compact('role'))
+        ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        $request->validate([
+            'name' => 'required',
+            
+        ]);
+    
+        $role->update($request->all());
+    
+        return redirect()->route('roles.index')
+                        ->with('success','Role mis à jour avec succès');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        $role->delete();
+    
+        return redirect()->route('roles.index')
+                        ->with('success','Role supprimé avec succès');
+    }
+}
