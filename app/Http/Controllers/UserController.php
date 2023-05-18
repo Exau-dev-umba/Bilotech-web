9fbecccb2276175f30379c31c6aebb9c2d88d0cf
espoir kakesa <espoirkakesa2@gmail.com>
2023-05-18 10:17:29 +0100

    ajout fichier userctrl

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index')->with('users', $users);
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+    }
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user, Role $role)
+    {
+        $roles = role::all();
+        return view('admin.users.edit', compact(['user', 'roles']));
+    }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->roles()->sync($request->roles);
+        return redirect()->route('users.index');
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->roles()->detach();
+        $user->delete();
+        return redirect()->route('users.index');
+    }
+}
+
+
+
+
