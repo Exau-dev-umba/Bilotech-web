fe5fd469c58fff4af3605bed12e2855bea5d483a
espoir kakesa <espoirkakesa2@gmail.com>
2023-05-18 15:27:23 +0100

    update file index in user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,37 +1,46 @@
 @extends('layouts.app')
-@section('content')
-<div class="card">
-    <div class="card-header text-center">
-    <h3 class="card-title">Liste des utlisateurs</h3>
-    <table class="table table-striped">
-    <thead>
-    <tr>
-    <th style="width: 10px">#</th>
-    <th>Nom</th>
-    <th>Email</th>
-    <th>Rôles</th>
-    <th>Action</th>
-    </tr>
-    </thead>
-            <tbody>
-                @foreach ($users as $user)
-                <tr>
-                    <td>  # </td>
-                <td> {{$user->name }} </td>
-                <td> {{$user->email }} </td>
-                <td> {{ implode(',' , $user->roles()->get()->pluck('name')->toArray()) }} </td>
-                <td>
-                    <a href=" {{route('users.edit', $user->id)}} "><button class=" btn btn-default"><i class="fas fa-pencil-alt"></i></button></a>
-                    <form action="{{route('users.destroy', $user->id)}}" method="post" class="d-inline">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-            </tbody>
-    </table>
-    </div>
+  @section('content')
+   <div class="card">
+    @can('manage-users')
+        <div class="card-header text-center">
+            <h3 class="card-title">Liste des utlisateurs</h3>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th style="width: 10px">#</th>
+                        <th>Nom</th>
+                        <th>Email</th>
+                        <th>Rôles</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                        <tr>
+                            <td>  # </td>
+                        <td> {{$user->name }} </td>
+                        <td> {{$user->email }} </td>
+                        <td> {{ implode(',' , $user->roles()->get()->pluck('name')->toArray()) }} </td>
+                        <td>
+                            @can('edit-users')
+                            <a href=" {{route('users.edit', $user->id)}} "><button class=" btn btn-default"><i class="fas fa-pencil-alt"></i></button></a>
+                            @endcan
+                            
+
+                            @can('delete-users')
+                            <form action="{{route('users.destroy', $user->id)}}" method="post" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                            </form>  
+                            @endcan
+                            
+                        </td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+            </table>
+        </div>
+    @endcan
     </div>
 @endsection
