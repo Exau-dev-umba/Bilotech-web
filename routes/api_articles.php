14c904a6364dd8a95f4706797492ae41431a52a2
baraka-bilali <barakabilali10@gmail.com>
2023-06-12 15:06:05 +0100

    Ajout de la documentation Swagger pour CategoryController

--- routes/api_articles.php
+++ routes/api_articles.php
@@ -4,7 +4,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\CategoryController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,12 @@ use App\Http\Controllers\Api\CategoryController;
 
 
 Route::get('articles', [ArticleController::class, 'index']);
+Route::get('articles/{article}', [ArticleController::class, 'show']);
 Route::get('articles/search', [ArticleController::class, 'search']);
 Route::get('categories', [CategoryController::class, 'index']);
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('articles/{id}/images', [ImageController::class, 'store']);
-    Route::apiResource('articles', ArticleController::class, ["as" => "api"])->except(['index']);
+    Route::post('category', [CategoryController::class, 'store']);
+    Route::apiResource('articles', ArticleController::class, ["as" => "api"])->except(['index', 'show']);
 });
 
